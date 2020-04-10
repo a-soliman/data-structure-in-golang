dc5e69ebe35aeeffdb646829695f578ab7c87b24
Ahmed Soliman <ahmed.soliman@programmer.net>
2020-04-10 07:46:06 -0700

    ll insert()

--- linkedlist.go
+++ linkedlist.go
@@ -39,10 +39,35 @@ func (ll *LinkedList) Append(value Item) {
 	ll.size++
 }
 
+// Insert inserts a new node at a given position in the linkedlist
+func (ll *LinkedList) Insert(i int, value Item) {
+	if i > ll.size || i < 1 {
+		return
+	}
+	if i == 1 {
+		ll.Append(value)
+		return
+	}
+	newNode := Node{value, nil}
+	nodeBefore := ll.head
+	step := 1
+	for {
+		if i == step+1 {
+			break
+		}
+		nodeBefore = nodeBefore.next
+		step++
+	}
+	newNode.next = nodeBefore.next
+	nodeBefore.next = &newNode
+	ll.size++
+}
+
 func main() {
 	ll := LinkedList{}
 	ll.Append(10)
 	ll.Append(12)
+	ll.Insert(2, 5)
 	fmt.Print(ll.head.next)
 
 }
